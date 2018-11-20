f575bce894df9c5240399141cb159d5700085cb3
Triskae <filipe.doutelsilva@hotmail.fr>
2018-11-20 23:05:33 +0100

    Added function to move the ship on all directions

--- src/game.js
+++ src/game.js
@@ -3,31 +3,68 @@
  * Under Licence MIT
  **/
 
-//Method where I can load my assets
-function preload () {
-    this.load.image('background', 'background.png');
+    //Creation of the first scene, the one wich contain the game scene
 
+let config = {
+        type: Phaser.AUTO,
+        width: 700,
+        height: 700,
+        scene: { preload: preload, create: create, update: update },
+    };
+
+let game = new Phaser.Game(config);
+
+
+let scene = new Phaser.Scene('backgroundScene');
+let cursor;
+let spaceship;
+
+//Method where I can load my assets
+function preload() {
+    //this.load.image('background', './assets/background.png');
+    this.load.spritesheet('spaceship', './assets/spaceship.png', {frameWidth: 32, frameHeight: 48});
 }
 
 //Method executed once just after preload
-function create (){
-    let bg = this.add.sprite(0, 0, 'background');
-    bg.setOrigin(0,0);
+function create() {
+    spaceship = this.add.sprite(340, 650, 'spaceship');
+
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('spaceship', {start: 0, end: 3}),
+        frameRate: 10,
+        repeat: -1
+    });
+    this.anims.create({
+        key: 'turn',
+        frames: [{key: 'spaceship', frame: 5}],
+        frameRate: 20
+    });
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('spaceship', {start: 8, end: 11}),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    cursors = this.input.keyboard.createCursorKeys();
 }
 
-function update () {
+function update() {
+
+    if (cursors.left.isDown) {
+        spaceship.anims.play('left', true);
+        spaceship.x -= 1;
+    }
+    else if (cursors.right.isDown) {
+        spaceship.anims.play('right', true);
+        spaceship.x += 1;
+    } else if (cursors.up.isDown) {
+        spaceship.y -= 1;
+    } else if (cursors.down.isDown) {
+        spaceship.y += 1;
+    }else {
+        spaceship.anims.play('turn');
+    }
 
 }
-
-let config = {
-    type: Phaser.AUTO,
-    width: 770,
-    height: 800,
-    scene:  {
-        preload: preload,
-        create: create,
-        update: update,
-    },
-};
-
-let game = new Phaser.Game(config);
