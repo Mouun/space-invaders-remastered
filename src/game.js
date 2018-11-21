b895d70ac0040f7c6cdaa5b57a91947251e41930
MarionPOESY <poesymarion@gmail.com>
2018-11-21 09:53:44 +0100

    Ajout ennemis

--- src/game.js
+++ src/game.js
@@ -18,11 +18,13 @@ let game = new Phaser.Game(config);
 let scene = new Phaser.Scene('backgroundScene');
 let cursor;
 let spaceship;
+let ennemis;
 
 //Method where I can load my assets
-function preload() {
-    //this.load.image('background', './assets/background.png');
+function preload() 
+{
     this.load.spritesheet('spaceship', './assets/spaceship.png', {frameWidth: 32, frameHeight: 48});
+    this.load.image('ennemis', './assets/ennemis.png', {frameWidth: 32, frameHeight: 48});
 }
 
 //Method executed once just after preload
@@ -48,10 +50,19 @@ function create() {
     });
 
     cursors = this.input.keyboard.createCursorKeys();
+
+    for(let j = 0; j < 700/4; j+=20) 
+    {
+        for(let i = 0; i < 700; i +=20) 
+        {
+            ennemis = this.add.sprite(i, j, 'ennemis');
+            ennemis.setScale(0.1);
+        }
+    }
 }
 
 function update() {
-
+    
     if (cursors.left.isDown) {
         spaceship.anims.play('left', true);
         spaceship.x -= 1;
