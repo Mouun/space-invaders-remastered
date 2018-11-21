f83bde536cb1ce69d2afdfe3f0f88deaddc9fab1
Triskae <filipe.doutelsilva@hotmail.fr>
2018-11-21 16:08:28 +0100

    Maintenant le vaisseau peu tirer

--- src/game.js
+++ src/game.js
@@ -22,23 +22,23 @@ let game = new Phaser.Game(config);
 let cursor;
 let spaceship;
 let bulletsGroup;
+let speed;
+let lastFired = 0;
+let bulletObject;
+let moveSpeed = 5;
+let shootRate = 200;
 
 //Method where I can load my assets
 function preload() {
     this.load.spritesheet('spaceship', './assets/spaceship.png', {frameWidth: 32, frameHeight: 48});
-    this.load.image('ennemis', './assets/ennemis.png', {frameWidth: 32, frameHeight: 48});
+    //this.load.image('ennemis', './assets/ennemis.png', {frameWidth: 32, frameHeight: 48});
+    bulletObject = this.load.image('bullet', './assets/bullet.png');
 }
 
 //Méthode exécutée juste après preload
 function create() {
     //Mise en place du vaisseau et de l'annimation
     spaceship = this.add.sprite(340, 650, 'spaceship');
-
-    console.log(game);
-    console.log(this);
-
-    //this.add.group();
-
     this.anims.create({
         key: 'left',
         frames: this.anims.generateFrameNumbers('spaceship', {start: 0, end: 3}),
@@ -59,43 +59,68 @@ function create() {
 
     cursors = this.input.keyboard.createCursorKeys();
 
-    for(let j = 0; j < 700/4; j+=20) 
-    {
-        for(let i = 0; i < 700; i +=20) 
-        {
-            ennemis = this.add.sprite(i, j, 'ennemis');
-            ennemis.setScale(0.1);
+    let Bullet = new Phaser.Class({
+
+        Extends: Phaser.GameObjects.Image,
+
+        initialize: function Bullet(scene) {
+            bulletObject = Phaser.GameObjects.Image.call(this, scene, 0, 0, 'bullet');
+
+            this.speed = Phaser.Math.GetSpeed(400, 1);
+        },
+
+        fire: function (x, y) {
+            this.setPosition(x, y - 50);
+
+            this.setActive(true);
+            this.setVisible(true);
+        },
+
+        update: function (time, delta) {
+            this.y -= this.speed * delta;
+
+            if (this.y < -50) {
+                this.setActive(false);
+                this.setVisible(false);
+            }
         }
-    }
+
+    });
+
+    bulletsGroup = this.add.group({
+        classType: Bullet,
+        maxSize: 10,
+        runChildUpdate: true
+    });
+
+    speed = Phaser.Math.GetSpeed(300, 1);
 }
 
-function update() {
-    
+function update(time, delta) {
+
     if (cursors.left.isDown) {
         if (spaceship.x < 0) {
             spaceship.x = 700;
         }
         spaceship.anims.play('left', true);
-        spaceship.x -= 1;
-    }
-    else if (cursors.right.isDown) {
+        spaceship.x -= moveSpeed;
+    } else if (cursors.right.isDown) {
         if (spaceship.x > 700) {
             spaceship.x = 0;
         }
         spaceship.anims.play('right', true);
-        spaceship.x += 1;
-    } else if (cursors.up.isDown) {
-        if (spaceship.y < 0) {
-            spaceship.y = 700;
-        }
-        spaceship.y -= 1;
-    } else if (cursors.down.isDown) {
-        if (spaceship.y > 700) {
-            spaceship.y = 0;
-        }
-        spaceship.y += 1;
+        spaceship.x += moveSpeed;
     } else {
         spaceship.anims.play('turn');
+    }
+
+    if (cursors.up.isDown && time > lastFired) {
+        let bullet = bulletsGroup.get();
+
+        if (bullet) {
+            bullet.fire(spaceship.x, spaceship.y);
+            lastFired = time + shootRate;
+        }
     }
 
 }
