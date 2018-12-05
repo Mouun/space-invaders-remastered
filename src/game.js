4ea39bac24fdf9b3bdfdb09112685f500fe6ab92
MarionPOESY <poesymarion@gmail.com>
2018-12-05 10:20:51 +0100

    Ennemis qui bougent

--- src/game.js
+++ src/game.js
@@ -6,8 +6,8 @@ let config = {
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: {y: 300},
-            debug: false
+            gravity: {y: 10},
+            debug: true
         }
     },
     scene: {
@@ -21,13 +21,13 @@ let game = new Phaser.Game(config);
 
 let cursor;
 let spaceship;
-let ennemis;
+let ennemi;
 let bulletsGroup;
 
 //Method where I can load my assets
 function preload() {
     this.load.spritesheet('spaceship', './assets/spaceship.png', {frameWidth: 32, frameHeight: 48});
-    this.load.image('ennemis', './assets/ennemis.png', {frameWidth: 32, frameHeight: 48});
+    this.load.image('ennemi', './assets/ennemis.png', {frameWidth: 32, frameHeight: 48});
 }
 
 
@@ -61,24 +61,35 @@ function create() {
 
     cursors = this.input.keyboard.createCursorKeys();
 
-    for(let j = 50; j < 700/4; j+=50) 
-    {
-        for(let i = 20; i < 700; i +=50) 
-        {
-            ennemis = this.add.sprite(i, j, 'ennemis');
-            ennemis.setScale(0.1);
-        }
-    }
+    ennemis = this.physics.add.group({
+        key: 'ennemi',
+        repeat: 20,
+        setXY: { x: 50, y: 20, stepX: 30 },
+        setScale: {x:0.05,y:0.05,stepX:0,stepY:0}
+    });
+
+    ennemis = this.physics.add.group({
+        key: 'ennemi',
+        repeat: 20,
+        setXY: { x: 50, y: 50, stepX: 30 },
+        setScale: {x:0.05,y:0.05,stepX:0,stepY:0}
+    });
+
+    ennemis = this.physics.add.group({
+        key: 'ennemi',
+        repeat: 20,
+        setXY: { x: 50, y: 80, stepX: 30 },
+        setScale: {x:0.05,y:0.05,stepX:0,stepY:0}
+    });
 }
 
-function descend() {
+function move() {
 
-    ennemis .y += 10;
+    ennemis.j += 0.4;
 
 }
 
 function update() {
-
 
     if (cursors.left.isDown) {
         if (spaceship.x < 0) {
