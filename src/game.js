acad1b14cf41ec6932df57ef0b21ae2b838bd956
Mouun <sorel.nadaud@outlook.com>
2018-12-09 17:18:38 +0100

    Génération des ennemis
    La fonction calculateMaxEnnemiesPerRow permet d'abord de calculer le nombre maximal d'ennemis pouvant être logés sur la largeur d'une ligne en prenant compte de l'espace entre chaque. La fonction generateEnemis permet de concrètement générer les lignes d'ennemis.
    Ce système de génération des ennemis doit évoluer pour prendre en compte la possibilité de générer des  lignes d'ennemis de types différents.

--- src/game.js
+++ src/game.js
@@ -31,7 +31,11 @@ let moveSpeed = 5;
 let shootRate = 200;
 let starfield;
 let scrollSpeed = 3;
-let nbEnemies = 10;
+let possiblePerRow = 0;
+let enemyWidth;
+let espaceBetweenEnemiesWidth = 50;
+let marginTopEnemies = 50;
+let nbRow = 4;
 let enemies;
 
 //Method where I can load my assets
@@ -49,15 +53,19 @@ function preload() {
     this.load.image('upgradeLvl4', './assets/player_level4.png', { frameWidth: 69, frameHeight: 110 });
     this.load.image('upgradeLvl4Left', './assets/player_level4_left.png', { frameWidth: 69, frameHeight: 110 });
     this.load.image('upgradeLvl4Right', './assets/player_level4_right.png', { frameWidth: 69, frameHeight: 110 });
-    this.load.image('enemy', './assets/ennemis.png', { frameWidth: 61, frameHeight: 64 });
-    this.load.image('bullet', './assets/laser_green.png');
+    this.load.image('bullet', './assets/round_laser_blue.png');
+    this.load.image('ennemi1', './assets/ennemi_1@0.75x.png', { frameWidth: 64, frameHeight: 48 });
+    this.load.image('ennemi2', './assets/ennemi_2@0.75x.png', { frameWidth: 70, frameHeight: 48 });
+    this.load.image('ennemi3', './assets/ennemi_3@0.75x.png', { frameWidth: 51, frameHeight: 49 });
+    this.load.image('ennemi4', './assets/ennemi_4@0.75x.png', { frameWidth: 93, frameHeight: 39 });
 }
 
 //Méthode exécutée juste après preload
 function create() {
 
-    let self = this;
+    enemyWidth = this.textures.list.ennemi1.source[0].width;
 
+    calculateMaxEnnemiesPerRow();
     starfield = this.add.tileSprite(gameWidth / 2, gameHeight / 2, gameWidth, gameHeight, "starfield");
 
     let Bullet = new Phaser.Class({
@@ -154,28 +162,58 @@ function update(time, delta) {
     }
 }
 
-function resize() {
-    let canvas = game.canvas;
-    let width = window.innerWidth;
-    let height = window.innerHeight;
-    let wratio = width / height, ratio = canvas.width / canvas.height;
+/**
+ * Fonction permettant de calculer le nombre maximum d'ennemis pouvant etre places sur une seule ligne
+ * ======== DETAIL DU CALCUL ========
+ * 1) Division de la largeur du canvas par la dimension d'un sprite d'ennemi pour connaitre le nombre d'ennemis
+ * que l'on peut placer sans les espacer (sans la partie décimale)
+ * ---> Math.trunc(gameWidth / enemyWidth)
+ *
+ * 2) Calcul du nombre de pixels necessaires pour afficher le nombre "division" d'ennemis si on considere une largeur de
+ * canvas infinie
+ * ---> Math.trunc(gameWidth / enemyWidth) * enemyWidth
+ *
+ * 3) Calcul du nombre d'espaces nécessaires (entre les ennemis et egalement a gauche et a droite de la ligne) toujours en
+ * considerant un canvas de largeur infinie
+ * ---> Math.trunc(gameWidth / enemyWidth) + 1
+ *
+ * 4) Calcul du nombre de pixels necessaires pour afficher le nombre "nbEspacesInitial" d'espaces si on considere une
+ * largeur de canvas infinie
+ * ---> Math.trunc(gameWidth / enemyWidth) + 1) * (espaceBetweenEnemiesWidth)
+ *
+ * 5) Calcul du nombre de pixels necessaires pour afficher une ligne entiere basee sur les resultats de
+ * "tailleTotaleEnnemisInitial" et "tailleTotaleEspacesInitial" en considerant une largeur infinie de canvas
+ * ---> ((Math.trunc(gameWidth / enemyWidth) * enemyWidth) + (Math.trunc(gameWidth / enemyWidth) + 1)) * (espaceBetweenEnemiesWidth))
+ *
+ * 6) Tant que le nombre de pixels d'une ligne (largeur de canvas infinie) est supérieur à la largeur de canvas
+ * definie, on soustrait la taille d'un ennemi + d'un espace a la taille d'une ligne avec largeur de canvas
+ * ininfie
+ *
+ * 7) Le nombre final est donc la taille totale d'une ligne initialement reduite divisee par la taille en pixels d'un
+ * ennemi additionnee a la taille en pixels d'un espace
+ *
+ * 8) On recalcule également l'espace entre chaque ennemi
+ * ==================================
+ */
+function calculateMaxEnnemiesPerRow() {
+    let tailleTotaleLigne = ((Math.trunc(gameWidth / enemyWidth) * enemyWidth) + (Math.trunc(gameWidth / enemyWidth) + 1)) * (espaceBetweenEnemiesWidth);
 
-    if (wratio < ratio) {
-        canvas.style.width = width + "px";
-        canvas.style.height = (width / ratio) + "px";
-    } else {
-        canvas.style.width = (height * ratio) + "px";
-        canvas.style.height = height + "px";
+    while (tailleTotaleLigne >= gameWidth) {
+        tailleTotaleLigne -= (enemyWidth + espaceBetweenEnemiesWidth);
     }
+
+    possiblePerRow = Math.trunc(tailleTotaleLigne / (enemyWidth + espaceBetweenEnemiesWidth));
+    espaceBetweenEnemiesWidth = (gameWidth - (enemyWidth * possiblePerRow)) / (possiblePerRow + 1);
 }
 
-function createEnemies(nbEnemies) {
-    for (let y = 0; y < 2; y++) {
-        for (let x = 0; x < 10; x++) {
-            let enemy = enemies.create(x * 100, y * 120, 'enemy');
+function generateEnemies() {
+    let espacementHorizontal = (enemyWidth / 2) + espaceBetweenEnemiesWidth;
+    for (let i = 0; i < nbRow; i++) {
+        for (let j = 0; j < possiblePerRow; j++) {
+            enemies.create(espacementHorizontal, marginTopEnemies, "ennemi1");
+            espacementHorizontal += enemyWidth + espaceBetweenEnemiesWidth;
         }
+        espacementHorizontal = (enemyWidth / 2) + espaceBetweenEnemiesWidth;
+        marginTopEnemies += 80;
     }
-
-    enemies.x = 100;
-    enemies.y = 50;
 }
